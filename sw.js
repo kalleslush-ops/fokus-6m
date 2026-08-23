1925ca6d5825a487b4e1fba54e3f59dd4c9b2ead
kalleslush-ops <kalleslush@gmail.com>
2026-08-23 15:15:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fokus6m-v6.0.0';
+const CACHE='fokus6m-v6.0.1';
 const CORE=['./','./index.html','./manifest.webmanifest','./logo-photo.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil((async()=>{const ks=await caches.keys();await Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim()})()));
